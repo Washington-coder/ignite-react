c3ccd1fa82388b88df0d9ce263c495081a2ae6b5
Washington <was.antonio.xy@gmail.com>
2024-12-28 12:32:50 -0400

    feat: structure of main screen

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,25 @@
 import { Header } from "./components/Header";
+import { Post } from "./Post";
+import styles from './App.module.css'
 
 export function App() {
 
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Ignite, Rocketseat"
+          />
+          <Post
+            author="Diego Fernandes"
+            content="Ignite, Rocketseat"
+          />
+        </main>
+      </div>
     </div>
   )
 }
