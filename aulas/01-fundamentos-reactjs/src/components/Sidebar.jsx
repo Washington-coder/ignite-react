f5a8dc38e27b9801b1fefb8b8a641e4356e3f123
Washington <was.antonio.xy@gmail.com>
2024-12-29 19:05:50 -0400

    feat: styling sidebar

--- aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,12 +10,14 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/82000177?v=4' />
                 <strong>Washington Moreno</strong>
                 <span>Web developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil.
                 </a>
             </footer>
