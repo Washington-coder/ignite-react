a1106aabfa609aaafc9d4594d8d2816ffe7820e5
Washington <was.antonio.xy@gmail.com>
2025-01-06 16:50:34 -0400

    chore: installing styled components

--- aulas/02-ignite-timer/src/App.tsx
+++ aulas/02-ignite-timer/src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { Button } from "./Components/Button/Button";
+import { defaultTheme } from "./themes/default";
+
 export function App() {
   return (
-    <>
-      Hello World
-    </>
+    <ThemeProvider theme={defaultTheme} > 
+      <Button variant="primary" />
+      <Button variant="secondary"/>
+    </ThemeProvider>
   )
 }
 
