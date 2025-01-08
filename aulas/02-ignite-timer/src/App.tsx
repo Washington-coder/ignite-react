f380638dabe25421a58e0197c971aa1470a81970
Washington <was.antonio.xy@gmail.com>
2025-01-07 21:42:21 -0400

    feat: adding global styles to the app

--- aulas/02-ignite-timer/src/App.tsx
+++ aulas/02-ignite-timer/src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "styled-components"
 import { Button } from "./Components/Button/Button";
-import { defaultTheme } from "./themes/default";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme} > 
       <Button variant="primary" />
       <Button variant="secondary"/>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
