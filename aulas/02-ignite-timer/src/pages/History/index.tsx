d504d80aec90526a03ffa5e373354d6cf0c81e82
Washington <was.antonio.xy@gmail.com>
2025-01-16 22:52:44 -0400

    feat: formatting date in history

--- aulas/02-ignite-timer/src/pages/History/index.tsx
+++ aulas/02-ignite-timer/src/pages/History/index.tsx
@@ -4,6 +4,8 @@ import {
     HistoryList,
     Status
 } from "./styles";
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from "date-fns/locale";
 import { CyclesContext } from "../../contexts/CyclesContext";
 
 export function History() {
@@ -31,7 +33,10 @@ export function History() {
                                 <tr key={cycle.id}>
                                     <td>{cycle.task}</td>
                                     <td>{cycle.minutesAmount} minutos</td>
-                                    <td>{cycle.startDate.toISOString()}</td>
+                                    <td>{formatDistanceToNow(cycle.startDate, {
+                                        addSuffix: true,
+                                        locale: ptBR
+                                    })}</td>
                                     <td>
                                         {cycle.finishedDate && (
                                             <Status statusColor="green">Concluído</Status>
