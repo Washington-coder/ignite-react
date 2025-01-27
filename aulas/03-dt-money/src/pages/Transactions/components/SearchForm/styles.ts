b64d9dfd391d9a6386275423916db81c68f6250d
Washington <was.antonio.xy@gmail.com>
2025-01-27 15:09:14 -0400

    feat: add cursor pointer to search btn

--- aulas/03-dt-money/src/pages/Transactions/components/SearchForm/styles.ts
+++ aulas/03-dt-money/src/pages/Transactions/components/SearchForm/styles.ts
@@ -30,6 +30,8 @@ export const SearchFormContainer = styled.form`
         font-weight: bold;
         border-radius: 6px;
         
+        cursor: pointer;
+        
         &:hover {
             background: ${props => props.theme['green-500']};
             border-color: ${props => props.theme['green-500']};
