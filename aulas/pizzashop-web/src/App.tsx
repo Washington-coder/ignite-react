c9855618a108721397852346ad1aa56b8a4675ed
Washington <was.antonio.xy@gmail.com>
2025-02-24 15:41:08 -0400

    feat: changing title according to routes

--- aulas/pizzashop-web/src/App.tsx
+++ aulas/pizzashop-web/src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 
 export function App() {
   return (
-    <RouterProvider router={router}>
-
-    </RouterProvider>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
