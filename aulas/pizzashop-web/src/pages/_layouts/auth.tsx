776deb69b5ee343f444799cb965d594044b91e31
Washington <was.antonio.xy@gmail.com>
2025-02-24 16:35:05 -0400

    feat: sign in layout structure

--- aulas/pizzashop-web/src/pages/_layouts/auth.tsx
+++ aulas/pizzashop-web/src/pages/_layouts/auth.tsx
@@ -1,13 +1,24 @@
 import { Outlet } from "react-router-dom";
+import { Pizza } from "lucide-react";
 
 export function AuthLayout() {
     return (
-        <div>
-            <h1>Autenticação</h1>
-
-            <div>
+        <div className="h-screen grid grid-cols-2">
+            {/* Coluna da esquerda */}
+            <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+                <div className="flex items-center gap-3 text-lg font-medium text-foreground">
+                    <Pizza className="h-5 w-5" />
+                    <span className="font-semibold">pizza.shop</span>
+                </div>
+                <footer className="text-sm">
+                    Painel do parceiro &copy; pizza.shop - 
+                </footer>
+            </div>
+            
+            {/* Coluna da direita */}
+            <div className="flex flex-col items-center justify-center">
                 <Outlet />
             </div>
         </div>
-    )
+    );
 }
