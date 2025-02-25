d462a22c698cc765fc57492582965c7bb48cf1b4
Washington <was.antonio.xy@gmail.com>
2025-02-25 15:35:59 -0400

    feat: adding toasts on submit signin

--- aulas/pizzashop-web/src/pages/auth/sign-in.tsx
+++ aulas/pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { toast } from "sonner";
 
 import { Label } from "../../components/ui/label";
 import { Button } from "../../components/ui/button";
@@ -16,8 +17,18 @@ export function SignIn() {
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>();
 
     async function handleSignIn(data: SignInForm) {
-        console.log(data)
-        await new Promise(resolve => setTimeout(resolve, 2000))
+        try {
+            console.log(data)
+            await new Promise(resolve => setTimeout(resolve, 2000))
+            toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+                action: {
+                    label: "Reenviar",
+                    onClick: () => handleSignIn(data)
+                }
+            });
+        } catch (error) {
+            toast.error("Credenciais inválidas.");            
+        }
     }
 
     return (
