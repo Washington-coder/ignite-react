b74e517dd1a8ce437fa7d2bf25108d07d7022c60
Washington <was.antonio.xy@gmail.com>
2025-02-26 15:40:32 -0400

    feat: header with navlinks

--- aulas/pizzashop-web/src/pages/_layouts/auth.tsx
+++ aulas/pizzashop-web/src/pages/_layouts/auth.tsx
@@ -3,7 +3,7 @@ import { Pizza } from "lucide-react";
 
 export function AuthLayout() {
     return (
-        <div className="h-screen grid grid-cols-2">
+        <div className="h-screen grid grid-cols-2 antialiased">
             {/* Coluna da esquerda */}
             <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
                 <div className="flex items-center gap-3 text-lg font-medium text-foreground">
